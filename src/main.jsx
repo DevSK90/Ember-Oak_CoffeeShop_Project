81c244fd88cf1225a472fe7a22470e03cae5fb90
Sahan Kalhara <142345492+SkDev22@users.noreply.github.com>
2026-07-26 19:42:13 +0530

    Updated base name

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/ember-oak">
+  <BrowserRouter basename="/Ember-Oak_CoffeeShop_Project">
     <App />
   </BrowserRouter>,
 );
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/ember-oak/",
+  base: "/Ember-Oak_CoffeeShop_Project/",
 });
